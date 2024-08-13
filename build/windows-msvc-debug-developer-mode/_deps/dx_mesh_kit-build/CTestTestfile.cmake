7743b30c26be18a2bcfb8083c3da082dbbc3ef85
rradarr <radoslaw.rzeczkowski2@gmail.com>
2024-08-13 22:26:41 +0200

    Initial commit with gitignore

--- build/windows-msvc-debug-developer-mode/_deps/dx_mesh_kit-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/programistyczne/Masters Project/build/windows-msvc-debug-developer-mode/_deps/dx_mesh_kit-src
-# Build directory: E:/programistyczne/Masters Project/build/windows-msvc-debug-developer-mode/_deps/dx_mesh_kit-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/windows-x64/_deps/dx_math-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/programistyczne/Masters Project/test2/build/windows-x64/_deps/dx_math-src
-# Build directory: E:/programistyczne/Masters Project/test2/build/windows-x64/_deps/dx_math-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
